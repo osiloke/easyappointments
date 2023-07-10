308b32d56e628ad5c0a88c9c25c9d01f337c336d
Osiloke Emoekpere <osi@progwebtech.com>
2023-07-10 17:03:46 +0100

    feat: rename app to pride

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -4,12 +4,12 @@
 
 <div id="about-page" class="container backend-page">
     <div id="about" class="col-lg-8 offset-lg-2">
-        
+
         <div class="text-center my-5">
-            <img src="<?= base_url('assets/img/logo.png') ?>" alt="Easy!Appointments Logo" class="mb-5">
-            
+            <img src="<?= base_url('assets/img/logo.png') ?>" alt="pride Logo" class="mb-5">
+
             <h3>
-                Easy!Appointments
+                pride
             </h3>
             <h6 class="text-primary">
                 Online Appointment Scheduler
@@ -36,28 +36,30 @@
         <h4 class="fw-light text-black-50 mb-3">
             <?= lang('support') ?>
         </h4>
-        
+
         <p>
             <?= lang('about_app_support') ?>
         </p>
 
         <div class="d-lg-flex justify-content-start flex-wrap alight-items-center mb-5">
             <div class="text-center w-50 mb-3 pe-3">
-                <a class="btn btn-outline-secondary d-block" href="https://easyappointments.org">
+                <a class="btn btn-outline-secondary d-block" href="https://pride.osiloke.com">
                     <i class="fas fa-external-link-alt me-2"></i>
                     <?= lang('official_website') ?>
-                </a>    
+                </a>
             </div>
 
             <div class="text-center w-50 mb-3 ps-3">
-                <a class="btn btn-outline-secondary d-block" href="https://groups.google.com/forum/#!forum/easy-appointments">
+                <a class="btn btn-outline-secondary d-block"
+                    href="https://groups.google.com/forum/#!forum/easy-appointments">
                     <i class="fas fa-external-link-alt me-2"></i>
                     <?= lang('support_group') ?>
                 </a>
             </div>
 
             <div class="text-center w-50 mb-3 pe-3">
-                <a class="btn btn-outline-secondary d-block" href="https://github.com/alextselegidis/easyappointments/issues">
+                <a class="btn btn-outline-secondary d-block"
+                    href="https://github.com/alextselegidis/easyappointments/issues">
                     <i class="fas fa-external-link-alt me-2"></i>
                     <?= lang('project_issues') ?>
                 </a>
@@ -85,9 +87,10 @@
         <p>
             <?= lang('about_app_license') ?>
         </p>
-        
+
         <div class="mb-5">
-            <a class="btn btn-outline-secondary d-block w-50 m-auto" href="https://www.gnu.org/licenses/gpl-3.0.en.html" target="_blank">
+            <a class="btn btn-outline-secondary d-block w-50 m-auto" href="https://www.gnu.org/licenses/gpl-3.0.en.html"
+                target="_blank">
                 <i class="fas fa-external-link-alt me-2"></i>
                 GPL-3.0
             </a>
